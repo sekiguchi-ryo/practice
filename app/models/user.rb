4294c4e50bef10c16342aa37e8c44fc6675a05fd
Sekiguchi Ryo <sekiguchi.ryo@omura.co.jp>
2024-02-07 16:44:21 +0900

    add carrierwave

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   # has_secure_password
+  mount_uploader :avatar, AvatarUploader
 end
