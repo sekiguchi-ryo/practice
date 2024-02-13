148a2d4fd0a3481b32a7a3fecbec56a0a83dc8f0
Sekiguchi Ryo <sekiguchi.ryo@omura.co.jp>
2024-02-13 16:55:16 +0900

    add maintenance mode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+    before_action :render_maintenance_page_except_for_whilte_list_ips, if: :maintenance_mode?
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
@@ -16,5 +17,21 @@ class ApplicationController < ActionController::Base
     def after_sign_out_path_for(resource)
         flash[:notice] = "ログアウトしました"
         new_user_session_path
+    end
+
+    def maintenance_mode?
+        Setting.find(1).mode
+    end
+
+    def render_maintenance_page_except_for_whilte_list_ips
+        ips_in_whitelist = Setting.find(1).ip_white_list.split(",")
+        return if ips_in_whitelist.include?(request.remote_ip)
+
+        render(
+            file: Rails.public_path.join("maintenance.html"),
+            content_type: "text/html",
+            layout: false,
+            status: :service_unavailable,
+        )
     end
 end
