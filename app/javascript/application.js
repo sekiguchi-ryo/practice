f28cb4fac97238c61e28b04b5a92d331718e8b28
Sekiguchi Ryo <sekiguchi.ryo@omura.co.jp>
2024-02-14 11:39:40 +0900

    modify views file

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "custom/check"
+// import "custom/check"
 import jQuery from "jquery"
 window.$ = window.jQuery = jQuery
 import("jquery-ui-dist")
