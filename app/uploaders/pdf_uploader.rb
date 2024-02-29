debb8e344fcf25457fb4b1194b9d9ac0c67ac261
Sekiguchi Ryo <sekiguchi.ryo@omura.co.jp>
2024-02-29 12:50:33 +0900

    add pdf to png

--- app/uploaders/pdf_uploader.rb
+++ app/uploaders/pdf_uploader.rb
@@ -1,7 +1,7 @@
 class PdfUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -44,4 +44,50 @@ class PdfUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg"
   # end
+
+  # version :thumb do
+  #   process convert: 'png'
+  # end
+
+  version :thumb do
+    # ここに`full_filename`を作るとそれがサムネイルのファイル名になります。
+    # このUploaderは画像とPDF両対応なのでPDFの時だけ拡張子を変更しています。
+    # `minimagick!`で`convert: :jpg`してるので拡張子変わってくれそうですが
+    # 残念ながら変わりません。
+    def full_filename(for_file)
+      name = super(for_file)
+      name = name.sub(/\.pdf$/, '.jpg') if for_file.ends_with?('.pdf')
+      name
+    end
+
+    # このメソッドでPDFの1ページ目を画像に変換しています。
+    process :pdf_first_page_to_jpg, if: :pdf?
+    
+    # サムネイルのサイズ
+    # process resize_to_limit: [280, 280]
+  end
+
+  private
+
+    def pdf?(new_file)
+      new_file.content_type == 'application/pdf'
+    end
+
+    def pdf_first_page_to_jpg
+      # 以前は`manipulate!`を使っていましたが`minimagick!`に変わりました。
+      # 引数には`ImageProcessing::Builder`がきます。
+      # `ImageProcessing::Builder`については
+      # すぐ下にリンクを貼りましたのでそちらを見てください。
+      minimagick! do |pdf|
+        pdf
+          # `append`は`convert`コマンドに渡す引数を渡せます。
+          # この二つのオプションを指定しないとものによっては背景が真っ黒になります。
+          .append('-background', 'white')
+          .append('-alpha', 'remove')
+          # `loader`の指定は`convert path/to/file.pdf[0] path/to/file.jpg`
+          # のようなコマンドを生成します。これでページ指定ができるようです。
+          .loader(page: 0)
+          .convert('jpg')
+      end
+    end
 end
